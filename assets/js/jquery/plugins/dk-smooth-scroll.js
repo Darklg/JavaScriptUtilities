dd4a59832a33d9a290853686682b7683ff194167
Darklg <kev.rocher@gmail.com>
2013-05-22 14:08:52 +0200

    jQuery — Smooth Scroll v 1.1 : Speed parameters

--- assets/js/jquery/plugins/dk-smooth-scroll.js
+++ assets/js/jquery/plugins/dk-smooth-scroll.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Smooth Scroll
- * Version: 1.0
+ * Version: 1.1
  * JavaScriptUtilities Smooth Scroll may be freely distributed under the MIT license.
  */
 
@@ -9,31 +9,45 @@
 ---------------------------------------------------------- */
 
 /*
-jQuery('[href^=#]').dkSmoothScroll();
+jQuery('[href^=#]').dkSmoothScroll({
+    duration: 500
+});
 */
 
 if (!jQuery.fn.dkSmoothScroll) {
     (function($) {
         var dkSmoothScroll = {
-            init : function(el){
+            params: {
+                duration: 500
+            },
+            init: function(el, params) {
                 this.el = el;
-                this.el.on('click', function(e){
+                this.setParams(params);
+                this.setEvents();
+            },
+            setParams: function(params) {
+                this.params = $.extend({}, this.params, params);
+            },
+            setEvents: function() {
+                var mthis = this;
+                this.el.on('click', function(e) {
                     var _this = jQuery(this),
+                        params = this.params,
                         href = _this.attr('href'),
                         target = jQuery(href);
-                    if(target.length > 0){
+                    if (target.length > 0) {
                         e.preventDefault();
                         var offsettop = target.offset().top;
                         jQuery('html, body').animate({
                             'scrollTop': offsettop
-                        }, 500);
+                        }, mthis.params.duration);
                     }
                 });
             }
         };
-        $.fn.dkSmoothScroll = function() {
+        $.fn.dkSmoothScroll = function(params) {
             this.each(function() {
-                $.extend({},dkSmoothScroll).init($(this));
+                $.extend({}, dkSmoothScroll).init($(this), params);
             });
             return this;
         };
