95391ee77cfdf77f28a614ce64ab1d9036e9ec21
Darklg <kev.rocher@gmail.com>
2013-05-30 22:39:04 +0200

    MooTools — Smooth Scroll v 1.1 : Speed parameters

--- assets/js/mootools/classes/dk-smooth-scroll.js
+++ assets/js/mootools/classes/dk-smooth-scroll.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Smooth Scroll
- * Version: 1.0
+ * Version: 1.1
  * JavaScriptUtilities Smooth Scroll may be freely distributed under the MIT license.
  */
 
@@ -13,23 +13,34 @@
  */
 
 /*
-new dkSmoothScroll($$('[href^=#]'));
+new dkSmoothScroll($$('[href^=#]'), {
+    duration: 500
+});
 */
-
 var dkSmoothScroll = new Class({
-    initialize : function(elements){
+    opt: {},
+    defaultOptions: {
+        duration: 500
+    },
+    initialize: function(elements, opt) {
         this.els = elements;
+        this.getOptions(opt);
         this.setEvents();
     },
-    setEvents : function(){
-        var mthis = this;
-        this.els.addEvent('click', function(e){
+    getOptions: function(opt) {
+        if (typeof opt == 'object') {
+            this.opt = Object.merge({}, this.defaultOptions, opt);
+        }
+    },
+    setEvents: function() {
+        var opt = this.opt;
+        this.els.addEvent('click', function(e) {
             var href = $(this).get('href'),
                 target = $$(href);
-            if(target[0]){
+            if (target[0]) {
                 e.preventDefault();
                 new Fx.Scroll(window, {
-                    duration: 500
+                    duration: opt.duration
                 }).toElement(target[0]);
             }
         });
