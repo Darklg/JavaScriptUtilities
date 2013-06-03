bcea79071b3986793e8896156d71e13e0bd6b1bb
Darklg <kev.rocher@gmail.com>
2013-06-03 22:10:56 +0200

    jQuery — Fake Placeholder v 1.0.2 : unification self & syntax

--- assets/js/jquery/plugins/fake-placeholder.js
+++ assets/js/jquery/plugins/fake-placeholder.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Fake Placeholder
- * Version: 1.0.1
+ * Version: 1.0.2
  * JavaScriptUtilities Fake Placeholder may be freely distributed under the MIT license.
  */
 
@@ -12,13 +12,13 @@
 jQuery('[placeholder]').FakePlaceholder();
 */
 
-if(!jQuery.fn.FakePlaceholder) {
+if (!jQuery.fn.FakePlaceholder) {
     (function($) {
         var FakePlaceholder = {
             init: function(el) {
                 this.el = el;
-                if(!("placeholder" in document.createElement("input"))) {
-                    if(!el.get('data-fakeplaceholder') && !el.hasClass('disable-placeholder')) {
+                if (!("placeholder" in document.createElement("input"))) {
+                    if (!el.get('data-fakeplaceholder') && !el.hasClass('disable-placeholder')) {
                         this.setElements();
                         this.showPlaceholder();
                         this.setEvents();
@@ -31,33 +31,33 @@ if(!jQuery.fn.FakePlaceholder) {
                 this.el.attr('placeholder', '');
             },
             setEvents: function() {
-                var mthis = this;
+                var self = this;
 
                 // Event on focus
                 this.el.on('focus', function() {
-                    mthis.hidePlaceholder();
+                    self.hidePlaceholder();
                 });
                 // Event on blur
                 this.el.on('blur', function() {
-                    mthis.showPlaceholder();
+                    self.showPlaceholder();
                 });
 
-                if(this.el.parents('form:first')) {
+                if (this.el.parents('form:first')) {
                     this.el.parents('form:first').on('submit', function(event) {
-                        if(mthis.el.val() == mthis.el.attr('data-fakeplaceholder')) {
-                            mthis.hidePlaceholder();
+                        if (self.el.val() == self.el.attr('data-fakeplaceholder')) {
+                            self.hidePlaceholder();
                         }
                     });
                 }
             },
             showPlaceholder: function() {
-                if(this.el.val() === '') {
+                if (this.el.val() === '') {
                     this.el.addClass('with-placeholder');
                     this.el.attr('value', this.el.attr('data-fakeplaceholder'));
                 }
             },
             hidePlaceholder: function() {
-                if(this.el.val() == this.el.attr('data-fakeplaceholder')) {
+                if (this.el.val() == this.el.attr('data-fakeplaceholder')) {
                     this.el.attr('value', '');
                     this.el.removeClass('with-placeholder');
                 }
