e3958c136ed503da1e34064bf67570480f3ad124
Darklg <kev.rocher@gmail.com>
2013-06-09 19:17:37 +0200

    jQuery Plugin template

---
+++ assets/js/jquery/template.js
@@ -0,0 +1,45 @@
+;if (!jQuery.fn.replaceThisStringWithAnID) {
+    (function($, window, document) {
+        // Main Class
+        var replaceThisStringWithAnID = {
+            defaultSettings: {},
+            settings: {},
+            init: function(el, settings) {
+                this.el = el;
+                this.getSettings(settings);
+                this.setElements();
+                this.setEvents();
+            },
+            // Obtaining user settings
+            getSettings: function(settings) {
+                if (typeof settings != 'object') {
+                    settings = {};
+                }
+                this.settings = $.extend(true, {}, this.defaultSettings, settings);
+            },
+            // Creating & setting elements
+            setElements: function() {
+                var self = this,
+                    settings = this.settings;
+            },
+            // Setting events
+            setEvents: function() {
+                var self = this,
+                    settings = this.settings;
+            }
+        };
+        // Using the replaceThisStringWithAnID class as a jQuery plugin
+        $.fn.replaceThisStringWithAnID = function(settings) {
+            this.each(function() {
+                var $this = $(this),
+                    dataPlugin = 'plugin_replaceThisStringWithAnID'.toLowerCase();
+                // Handling duplicate calls
+                if (!$this.hasClass(dataPlugin)) {
+                    $.extend(true, {}, replaceThisStringWithAnID).init($this, opt);
+                    $this.addClass(dataPlugin);
+                }
+            });
+            return this;
+        };
+    })(jQuery, window, document);
+}
