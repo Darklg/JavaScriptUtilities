9b3253d495e764d135f85cd117ff1d9a25a7c104
Darklg <kev.rocher@gmail.com>
2013-08-13 22:55:29 +0200

    jQuery - Tabs v 1.1 : Callback, settings syntax, better index number and code refresh

--- assets/js/jquery/plugins/dk-jsu-tabs.js
+++ assets/js/jquery/plugins/dk-jsu-tabs.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Tabs
- * Version: 1.0.2.1
+ * Version: 1.1
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Tabs may be freely distributed under the MIT license.
  */
@@ -18,45 +18,61 @@ jQuery('.tab-triggers').dkJSUTabs({
 if (!jQuery.fn.dkJSUTabs) {
     (function($) {
         var dkJSUTabs = {
-            defaultOpt: {
-                classCurrent: 'is-current'
+            defaultSettings: {
+                classCurrent: 'is-current',
+                callBackTab: function(i) {}
             },
-            opt: {},
-            init: function(els, opt) {
+            settings: {},
+            init: function(els, settings) {
                 this.els = els;
                 var els0 = els.eq(0);
                 if (els0.hasClass('has-dk-jsu-tabs')) {
                     return;
                 }
                 els0.addClass('has-dk-jsu-tabs');
-                this.getOptions(opt);
-                if (this.opt.targets) {
+                this.getSettings(settings);
+                if (this.settings.targets) {
                     this.setEvents();
                     this.showTab(0);
                 }
             },
-            getOptions: function(opt) {
-                this.opt = $.extend(true, {}, this.defaultOpt, opt);
+            // Obtaining user settings
+            getSettings: function(settings) {
+                if (typeof settings != 'object') {
+                    settings = {};
+                }
+                this.settings = $.extend(true, {}, this.defaultSettings, settings);
             },
             setEvents: function() {
-                var self = this;
+                var self = this,
+                    settings = this.settings;
+
+                // Set element index
+                self.els.each(function(i) {
+                    $(this).attr('data-tabsi', i);
+                });
+
                 self.els.on('click', function(e) {
+                    var i = parseInt($(this).attr('data-tabsi'), 10);
                     e.preventDefault();
-                    self.showTab($(this).index());
+                    self.showTab(i);
                 });
             },
             showTab: function(i) {
-                var opt = this.opt,
-                    clss = opt.classCurrent,
-                    targets = opt.targets;
-                this.els.removeClass(clss);
-                targets.removeClass(clss);
-                this.els.eq(i).addClass(clss);
-                targets.eq(i).addClass(clss);
+                var settings = this.settings,
+                    classCurrent = settings.classCurrent,
+                    targets = settings.targets;
+                settings.callBackTab(i);
+                if (this.els.eq(i) && targets.eq(i)) {
+                    this.els.removeClass(classCurrent);
+                    targets.removeClass(classCurrent);
+                    this.els.eq(i).addClass(classCurrent);
+                    targets.eq(i).addClass(classCurrent);
+                }
             }
         };
-        $.fn.dkJSUTabs = function(opt) {
-            $.extend(true, {}, dkJSUTabs).init(this, opt);
+        $.fn.dkJSUTabs = function(settings) {
+            $.extend(true, {}, dkJSUTabs).init(this, settings);
             return this;
         };
     })(jQuery);
