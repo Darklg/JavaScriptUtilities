3e66d30ace451cef6aa4326be2892c4963352c7b
Darklg <kev.rocher@gmail.com>
2013-09-03 07:55:57 +0200

    Mootools - Fake Placeholder v 1.1 : one object per element

--- assets/js/mootools/classes/fake-placeholder.js
+++ assets/js/mootools/classes/fake-placeholder.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Fake Placeholder
- * Version: 1.0.1
+ * Version: 1.1
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Fake Placeholder may be freely distributed under the MIT license.
  */
@@ -10,60 +10,65 @@
 ---------------------------------------------------------- */
 
 /*
-new FakePlaceholder($$('[placeholder]'));
+$$('[placeholder]').each(function(el) {
+    new FakePlaceholder(el);
+});
 */
+
 var FakePlaceholder = new Class({
-    initialize: function(elementz) {
-        var self = this;
-        this.elementz = elementz;
+    initialize: function(el) {
+        // Duplicate calls
+        var controlClass = 'moo_fakeplaceholder';
+        if (!el || el.hasClass(controlClass)) {
+            return;
+        }
+        el.addClass(controlClass);
+        this.el = el;
         if (!("placeholder" in document.createElement("input"))) {
-            elementz.each(function(el) {
-                if (!el.get('data-fakeplaceholder') && !el.hasClass('disable-placeholder')) {
-                    self.setElements(el);
-                    self.showPlaceholder(el);
-                    self.setEvents(el);
-                }
-            });
+            if (!this.el.hasClass('disable-placeholder')) {
+                this.setElements();
+                this.showPlaceholder();
+                this.setEvents();
+            }
         }
     },
-    setElements: function(el) {
-        el.set('data-inputtype', el.get('type'));
-        el.set('data-fakeplaceholder', el.get('placeholder'));
-        el.set('placeholder', '');
+    setElements: function() {
+        this.el.set('data-inputtype', this.el.get('type'));
+        this.el.set('data-fakeplaceholder', this.el.get('placeholder'));
+        this.el.set('placeholder', '');
     },
     setEvents: function(el) {
-        var self = this,
-            tel = el;
+        var self = this;
 
         // Event on focus
-        el.addEvent('focus', function(e) {
+        this.el.addEvent('focus', function(e) {
             self.hidePlaceholder(this);
         });
 
         // Event on blur
-        el.addEvent('blur', function(e) {
+        this.el.addEvent('blur', function(e) {
             self.showPlaceholder(this);
         });
 
         // Fix on form submit
-        if (el.getParent('form')) {
-            el.getParent('form').addEvent('submit', function() {
-                if (tel.get('value') == tel.get('data-fakeplaceholder')) {
-                    self.hidePlaceholder(tel);
+        if (this.el.getParent('form')) {
+            this.el.getParent('form').addEvent('submit', function() {
+                if (self.el.get('value') == self.el.get('data-fakeplaceholder')) {
+                    self.hidePlaceholder();
                 }
             });
         }
     },
-    showPlaceholder: function(el) {
-        if (el.get('value') === '') {
-            el.addClass('with-placeholder');
-            el.set('value', el.get('data-fakeplaceholder'));
+    showPlaceholder: function() {
+        if (this.el.get('value') === '') {
+            this.el.addClass('with-placeholder');
+            this.el.set('value', this.el.get('data-fakeplaceholder'));
         }
     },
-    hidePlaceholder: function(el) {
-        if (el.get('value') == el.get('data-fakeplaceholder')) {
-            el.set('value', '');
-            el.removeClass('with-placeholder');
+    hidePlaceholder: function() {
+        if (this.el.get('value') == this.el.get('data-fakeplaceholder')) {
+            this.el.set('value', '');
+            this.el.removeClass('with-placeholder');
         }
     }
 });
