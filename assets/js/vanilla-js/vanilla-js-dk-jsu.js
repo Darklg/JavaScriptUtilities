70594d8df42bf8dbd7e80d2665fcf684dca3a4ea
Darklg <kev.rocher@gmail.com>
2013-10-25 23:56:30 +0200

    Vanilla JS v 1.2 - Compatibility tests

--- assets/js/vanilla-js/vanilla-js-dk-jsu.js
+++ assets/js/vanilla-js/vanilla-js-dk-jsu.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS
- * Version: 1.1
+ * Version: 1.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -31,6 +31,31 @@ window.domReady = function(func) {
     else {
         func();
     }
+};
+
+/* ----------------------------------------------------------
+  Tests
+---------------------------------------------------------- */
+
+var dkJSUTests = {
+    canvas: function() {
+        var elem = document.createElement('canvas');
+        return !!(elem.getContext && elem.getContext('2d'));
+    },
+    localStorage: function() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        }
+        catch (e) {
+            return false;
+        }
+    },
+    offline: function() {
+        return !!window.applicationCache;
+    },
+    touch: function() {
+        return 'ontouchstart' in window || 'onmsgesturechange' in window;
+    },
 };
 
 /* ----------------------------------------------------------
