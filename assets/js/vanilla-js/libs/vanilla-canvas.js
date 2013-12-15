334de49277b30684c78bab9cf12920c5320726fa
Darklg <kev.rocher@gmail.com>
2013-12-15 18:46:34 +0100

    Vanilla - Canvas v 0.2 : Cover video

--- assets/js/vanilla-js/libs/vanilla-canvas.js
+++ assets/js/vanilla-js/libs/vanilla-canvas.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Canvas
- * Version: 1.0
+ * Version: 2.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -17,44 +17,99 @@ var dkJSUCanvas = function(canvas) {
     this.canvas.height = this.cH;
     this.canvas.width = this.cW;
 
+    /* ----------------------------------------------------------
+      Cover Media
+    ---------------------------------------------------------- */
+
+    // Cover Video
+    this.coverVideo = function(video) {
+        var self = this;
+
+        this.video = video || this.getChildIf('VIDEO');
+        if (!this.video) {
+            return;
+        }
+
+        // Obtain dimensions
+        this.dim = this.getCoverDimensions(this.video);
+
+        this.video.addEventListener('play', function() {
+            self.drawVideo();
+        });
+
+    };
+
+    this.drawVideo = function() {
+        var self = this;
+
+        // Draw video
+        this.context.drawImage(this.video, this.dim.left, this.dim.top, this.dim.width, this.dim.height);
+
+        // Test if video is paused
+        if (this.video.paused || this.video.ended) {
+            return false;
+        }
+
+        // Launch next frame
+        setTimeout(function() {
+            self.drawVideo();
+        }, 40);
+    };
+
     // Cover Image
     this.coverImage = function(image) {
-        // Set Positions
-        var left = 0,
-            top = 0,
-            width = 10,
-            height = 10;
 
-        // Search for a child image
+        image = image || this.getChildIf('IMG');
         if (!image) {
-            // Test if canvas has a child, with a img tagName.
-            if (!canvas.children[0] || canvas.children[0].tagName != 'IMG') {
-                return;
-            }
-            image = canvas.children[0];
+            return;
         }
 
-        this.image = image;
-
-        // Get Image size
-        this.iH = this.image.height;
-        this.iW = this.image.width;
-        this.iRatio = this.iH / this.iW;
-
-        // Get image position
-        if (this.cRatio < this.iRatio) {
-            width = this.cW;
-            height = width * this.iRatio;
-            top = 0 - (height - this.cH) / 2;
-        }
-        else {
-            height = this.cH;
-            width = height / this.iRatio;
-            left = 0 - (width - this.cW) / 2;
-        }
+        // Obtain dimensions
+        var dim = this.getCoverDimensions(image);
 
         // Draw image
-        this.context.drawImage(this.image, left, top, width, height);
+        this.context.drawImage(image, dim.left, dim.top, dim.width, dim.height);
+
+    };
+
+    /* ----------------------------------------------------------
+      Utilities
+    ---------------------------------------------------------- */
+
+    this.getChildIf = function(tagName) {
+        // Test if canvas has a child, with a img tagName.
+        if (!canvas.children[0] || canvas.children[0].tagName != tagName) {
+            return false;
+        }
+        return canvas.children[0];
+    };
+
+    // Get Cover dimensions for an element
+    this.getCoverDimensions = function(image) {
+        // Get Image size
+        var iReturn = {
+            left: 0,
+            top: 0,
+            width: 10,
+            height: 10
+        },
+            iH = image.height,
+            iW = image.width,
+            iRatio = iH / iW;
+
+        // Get image position
+        if (this.cRatio < iRatio) {
+            iReturn.width = this.cW;
+            iReturn.height = iReturn.width * iRatio;
+            iReturn.top = 0 - (iReturn.height - this.cH) / 2;
+        }
+        else {
+            iReturn.height = this.cH;
+            iReturn.width = iReturn.height / iRatio;
+            iReturn.left = 0 - (iReturn.width - this.cW) / 2;
+        }
+
+        return iReturn;
 
     };
 };
--- assets/js/vanilla-js/vanilla-js.js
+++ assets/js/vanilla-js/vanilla-js.js
@@ -99,6 +99,124 @@ if (!Array.prototype.each) {
 /*---*/
 
 /*
+ * Plugin Name: Vanilla-JS Canvas
+ * Version: 2.0
+ * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
+ * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
+ */
+
+var dkJSUCanvas = function(canvas) {
+    // Set Vars
+    this.canvas = canvas;
+    this.cH = this.canvas.clientHeight;
+    this.cW = this.canvas.clientWidth;
+    this.cRatio = this.cH / this.cW;
+
+    // Set canvas
+    this.context = this.canvas.getContext("2d");
+    this.canvas.height = this.cH;
+    this.canvas.width = this.cW;
+
+    /* ----------------------------------------------------------
+      Cover Media
+    ---------------------------------------------------------- */
+
+    // Cover Video
+    this.coverVideo = function(video) {
+        var self = this;
+
+        this.video = video || this.getChildIf('VIDEO');
+        if (!this.video) {
+            return;
+        }
+
+        // Obtain dimensions
+        this.dim = this.getCoverDimensions(this.video);
+
+        this.video.addEventListener('play', function() {
+            self.drawVideo();
+        });
+
+    };
+
+    this.drawVideo = function() {
+        var self = this;
+
+        // Draw video
+        this.context.drawImage(this.video, this.dim.left, this.dim.top, this.dim.width, this.dim.height);
+
+        // Test if video is paused
+        if (this.video.paused || this.video.ended) {
+            return false;
+        }
+
+        // Launch next frame
+        setTimeout(function() {
+            self.drawVideo();
+        }, 40);
+    };
+
+    // Cover Image
+    this.coverImage = function(image) {
+
+        image = image || this.getChildIf('IMG');
+        if (!image) {
+            return;
+        }
+
+        // Obtain dimensions
+        var dim = this.getCoverDimensions(image);
+
+        // Draw image
+        this.context.drawImage(image, dim.left, dim.top, dim.width, dim.height);
+
+    };
+
+    /* ----------------------------------------------------------
+      Utilities
+    ---------------------------------------------------------- */
+
+    this.getChildIf = function(tagName) {
+        // Test if canvas has a child, with a img tagName.
+        if (!canvas.children[0] || canvas.children[0].tagName != tagName) {
+            return false;
+        }
+        return canvas.children[0];
+    };
+
+    // Get Cover dimensions for an element
+    this.getCoverDimensions = function(image) {
+        // Get Image size
+        var iReturn = {
+            left: 0,
+            top: 0,
+            width: 10,
+            height: 10
+        },
+            iH = image.height,
+            iW = image.width,
+            iRatio = iH / iW;
+
+        // Get image position
+        if (this.cRatio < iRatio) {
+            iReturn.width = this.cW;
+            iReturn.height = iReturn.width * iRatio;
+            iReturn.top = 0 - (iReturn.height - this.cH) / 2;
+        }
+        else {
+            iReturn.height = this.cH;
+            iReturn.width = iReturn.height / iRatio;
+            iReturn.left = 0 - (iReturn.width - this.cW) / 2;
+        }
+
+        return iReturn;
+
+    };
+};
+
+/*---*/
+
+/*
  * Plugin Name: Vanilla-JS Classes
  * Version: 1.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
