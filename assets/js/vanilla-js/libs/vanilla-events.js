63e70609bd6d7cb9ff35ad75e2385884c24267a8
Darklg <kev.rocher@gmail.com>
2014-01-02 18:04:41 +0100

    Vanilla - Events v 1.0

---
+++ assets/js/vanilla-js/libs/vanilla-events.js
@@ -0,0 +1,38 @@
+/*
+ * Plugin Name: Vanilla-JS Events
+ * Version: 1.0
+ * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
+ * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
+ */
+
+/* ----------------------------------------------------------
+  Domready
+---------------------------------------------------------- */
+
+/* From the amazing Dustin Diaz : http://www.dustindiaz.com/smallest-domready-ever */
+// "!document.body" check ensures that IE fires domReady correctly
+window.domReady = function(func) {
+    if (/in/.test(document.readyState) || !document.body) {
+        setTimeout(function() {
+            window.domReady(func);
+        }, 9);
+    }
+    else {
+        func();
+    }
+};
+
+/* ----------------------------------------------------------
+  Add Event
+---------------------------------------------------------- */
+
+window.addEvent = function(el, eventName, callback) {
+    if (el.addEventListener) {
+        el.addEventListener(eventName, callback, false);
+    }
+    else if (el.attachEvent) {
+        el.attachEvent("on" + eventName, function(e) {
+            return callback.call(el, e);
+        });
+    }
+};
