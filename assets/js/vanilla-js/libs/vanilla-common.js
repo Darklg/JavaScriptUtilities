162c9a46fea9e1a440efc2b817b8fae430332340
Darklg <kev.rocher@gmail.com>
2014-01-02 18:04:41 +0100

    Vanilla - Events v 1.0

--- assets/js/vanilla-js/libs/vanilla-common.js
+++ assets/js/vanilla-js/libs/vanilla-common.js
@@ -5,22 +5,6 @@
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
 
-/* Domready
--------------------------- */
-
-/* From the amazing Dustin Diaz : http://www.dustindiaz.com/smallest-domready-ever */
-// "!document.body" check ensures that IE fires domReady correctly
-window.domReady = function(func) {
-    if (/in/.test(document.readyState) || !document.body) {
-        setTimeout(function() {
-            window.domReady(func);
-        }, 9);
-    }
-    else {
-        func();
-    }
-};
-
 /* Tests
 -------------------------- */
 
