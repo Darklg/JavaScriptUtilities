244aedbc4202dc2cf498b480352d769dc742b163
Darklg <kev.rocher@gmail.com>
2014-01-28 23:39:35 +0100

    Vanilla - Common v 1.4.2 : get body scroll & get window inner height

--- assets/js/vanilla-js/libs/vanilla-common.js
+++ assets/js/vanilla-js/libs/vanilla-common.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Common
- * Version: 1.4.1
+ * Version: 1.4.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -58,4 +58,18 @@ var callOnImgLoad = function(url, callback) {
 
     // Set image load
     img.src = url;
+};
+
+/* Get BODY Scroll Top
+-------------------------- */
+
+var getBodyScrollTop = function() {
+    return document.documentElement.scrollTop || document.body.scrollTop;
+};
+
+/* Get Window Inner Height
+-------------------------- */
+
+var getWindowInnerHeight = function() {
+    return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 };
