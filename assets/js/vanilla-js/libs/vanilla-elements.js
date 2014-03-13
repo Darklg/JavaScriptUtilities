419cd8f92b78ee2e6449b1875c06dae55f393e72
Darklg <kev.rocher@gmail.com>
2014-03-13 22:35:30 +0100

    Vanilla - Elements v 2.0 : Set Styles, Get Children, Wrap Element

--- assets/js/vanilla-js/libs/vanilla-elements.js
+++ assets/js/vanilla-js/libs/vanilla-elements.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Elements
- * Version: 1.1
+ * Version: 2.0
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -52,3 +52,74 @@ if (!Element.prototype.toggleDisplay) {
         return Element.toggleDisplay(this);
     };
 }
+
+/* ----------------------------------------------------------
+  Set styles
+---------------------------------------------------------- */
+
+var elementSetStyles = function(element, styles) {
+    var elStyle = element.style,
+        property,
+        propertyName,
+        setCamelCase = function(str) {
+            /* http://stackoverflow.com/a/6661012 */
+            return str.replace(/-([a-z])/g, function(g) {
+                return g[1].toUpperCase();
+            });
+        };
+    styles = styles || {};
+
+    for (property in styles) {
+        propertyName = setCamelCase(property);
+        element.style[propertyName] = styles[property];
+    }
+};
+
+if (!Element.prototype.setStyles) {
+    Element.prototype.setStyles = function(styles) {
+        return elementSetStyles(this, styles);
+    };
+}
+
+/* ----------------------------------------------------------
+  Get Children
+---------------------------------------------------------- */
+
+var getChildren = function(element) {
+    var childNodes = element.childNodes,
+        children = [];
+
+    var arLength = childNodes.length;
+    for (var i = 0; i < arLength; i++) {
+        if (childNodes[i].nodeType == 1) {
+            children.push(childNodes[i]);
+        }
+    }
+
+    return children;
+};
+
+if (!Element.prototype.getChildren) {
+    Element.prototype.getChildren = function() {
+        return getChildren(this);
+    };
+}
+
+/* ----------------------------------------------------------
+  Wrap Element
+---------------------------------------------------------- */
+
+var wrapElement = function(element, tagName) {
+    tagName = tagName || 'div';
+    tagName = tagName.toLowerCase();
+    var wrapper = document.createElement(tagName);
+
+    if (element.nextSibling) {
+        element.parentNode.insertBefore(wrapper, element.nextSibling);
+    }
+    else {
+        element.parentNode.appendChild(wrapper);
+    }
+    wrapper.appendChild(element);
+    return wrapper;
+};
