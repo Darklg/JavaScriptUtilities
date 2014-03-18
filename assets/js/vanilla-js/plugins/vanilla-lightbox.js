e1034169dd19b778a4de38dd9dc7b327dd89971a
Darklg <kev.rocher@gmail.com>
2014-03-18 23:48:41 +0100

    Vanilla - Lightbox v 0.1

---
+++ assets/js/vanilla-js/plugins/vanilla-lightbox.js
@@ -0,0 +1,69 @@
+/*
+ * Plugin Name: Lightbox
+ * Version: 0.1
+ * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
+ * JavaScriptUtilities Slider may be freely distributed under the MIT license.
+ * Required: Vanilla Events, Vanilla Selectors, Vanilla Classes
+ */
+
+var vanillaLightbox = function(settings) {
+    var self = this;
+    self.els = {
+        box: false,
+        filter: false,
+        content: false
+    };
+    self.defaultSettings = {};
+    self.init = function(settings) {
+        self.getSettings(settings);
+        self.setElements();
+        self.setEvents();
+    };
+    self.setElements = function() {
+        var els = self.els;
+        // Create box
+        els.box = document.createElement("div")
+            .addClass('lightbox')
+            .addClass('fluid-lightbox')
+            .addClass('lb-is-hidden');
+        // Create filter
+        els.filter = document.createElement("div")
+            .addClass('lightbox-filter');
+        els.box.appendChild(els.filter);
+        // Create content
+        els.content = document.createElement("div")
+            .addClass('lightbox-content');
+        els.box.appendChild(els.content);
+        // Inject lightbox
+        document.body.appendChild(els.box);
+    };
+    self.setEvents = function() {
+        self.els.filter.addEvent('click', function(e) {
+            window.eventPreventDefault(e);
+            self.close();
+        });
+    };
+    self.setContent = function(content) {
+        self.els.content.innerHTML = content;
+    };
+    self.open = function() {
+        self.els.box.removeClass('lb-is-hidden');
+    };
+    self.close = function() {
+        self.els.box.addClass('lb-is-hidden');
+    };
+    self.init(settings);
+    return self;
+};
+
+vanillaLightbox.prototype.getSettings = function(settings) {
+    self.settings = {};
+    // Set default values
+    for (var attr in self.defaultSettings) {
+        self.settings[attr] = self.defaultSettings[attr];
+    }
+    // Set new values
+    for (var attr2 in settings) {
+        self.settings[attr2] = settings[attr2];
+    }
+};
