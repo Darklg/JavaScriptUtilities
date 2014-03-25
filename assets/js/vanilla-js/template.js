33ff4566da3e7e89cfbc0c5dbd524f234e1c01e9
Darklg <kev.rocher@gmail.com>
2014-03-25 09:22:19 +0100

    Vanilla - Template with separated getSettings(), setElements function.

--- assets/js/vanilla-js/template.js
+++ assets/js/vanilla-js/template.js
@@ -1,25 +1,30 @@
-;if (!replaceThisStringWithAnID) {
-    var replaceThisStringWithAnID = function(settings) {
-        var self = this;
-        self.defaultSettings = {};
-        self.init = function(settings) {
-            self.getSettings(settings);
-            self.setEvents();
-        };
-        self.getSettings = function(settings) {
-            self.settings = {};
-            // Set default values
-            for (var attr in self.defaultSettings) {
-                self.settings[attr] = self.defaultSettings[attr];
-            }
-            // Set new values
-            for (var attr2 in settings) {
-                self.settings[attr2] = settings[attr2];
-            }
-        };
-        self.setEvents = function() {
-
-        };
-        self.init(settings);
+var replaceThisStringWithAnID = function(settings) {
+    var self = this;
+    self.defaultSettings = {};
+    self.init = function(settings) {
+        self.getSettings(settings);
+        self.setElements();
+        self.setEvents();
     };
-}
+    self.setElements = function() {
+
+    };
+    self.setEvents = function() {
+
+    };
+    self.init(settings);
+    return self;
+};
+
+/* Get Settings */
+replaceThisStringWithAnID.prototype.getSettings = function(settings) {
+    self.settings = {};
+    // Set default values
+    for (var attr in self.defaultSettings) {
+        self.settings[attr] = self.defaultSettings[attr];
+    }
+    // Set new values
+    for (var attr2 in settings) {
+        self.settings[attr2] = settings[attr2];
+    }
+};
