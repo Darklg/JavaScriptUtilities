e97c9e6b8e45f6a115f7687ceb96966c7f5ac54b
Darklg <kev.rocher@gmail.com>
2014-03-28 00:01:54 +0100

    Vanilla - Slider v 0.3 : Pagination, Navigation, Default presentation, proto get settings

--- assets/js/vanilla-js/plugins/vanilla-slider.js
+++ assets/js/vanilla-js/plugins/vanilla-slider.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Slider
- * Version: 0.2
+ * Version: 0.3
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Slider may be freely distributed under the MIT license.
  * Required: Vanilla Events, Vanilla Elements, Vanilla Classes
@@ -17,35 +17,42 @@ new vanillaSlider({
 */
 
 var vanillaSlider = function(settings) {
-    var self = this,
-        defaultSettings = {
-            autoSlide: true,
-            autoSlideDuration: 7000,
-            nbSlides: 0,
-            currentSlide: 0,
-            slides: false,
-            slider: false,
-            transition: function(oldSlide, newSlide, oldNb, nb, self) {
+    var self = this;
+    self.defaultSettings = {
+        autoSlide: true,
+        autoSlideDuration: 7000,
+        createPagination: true,
+        createNavigation: true,
+        displayPagination: true,
+        displayNavigation: true,
+        nbSlides: 0,
+        slides: false,
+        slider: false,
+        transition: function(oldSlide, newSlide, oldNb, nb, self) {
+            newSlide.setStyles({
+                'z-index': 2,
+            });
+            newSlide.addClass('active-slide');
+            setTimeout(function() {
                 newSlide.setStyles({
-                    'z-index': 2,
+                    'z-index': 1,
                 });
-                newSlide.addClass('active-slide');
-                setTimeout(function() {
-                    newSlide.setStyles({
-                        'z-index': 1,
+                if (oldNb !== nb) {
+                    oldSlide.setStyles({
+                        'z-index': 0
                     });
-                    if (oldNb !== nb) {
-                        oldSlide.setStyles({
-                            'z-index': 0
-                        });
-                        oldSlide.removeClass('active-slide');
-                    }
-                    // Authorizing a new slide
-                    self.canSlide = 1;
-                }, 300);
-            }
-        };
+                    oldSlide.removeClass('active-slide');
+                }
+                // Authorizing a new slide
+                self.canSlide = 1;
+            }, 300);
+        }
+    };
     self.canSlide = 1;
+    self.currentSlide = 0;
+    self.navigationEl = false;
+    self.pagers = false;
+    self.paginationEl = false;
     self.autoSlideTimeout = false;
     self.init = function(settings) {
         self.getSettings(settings);
@@ -56,26 +63,18 @@ var vanillaSlider = function(settings) {
         self.setEvents();
         self.goToSlide('init');
     };
-    self.getSettings = function(settings) {
-        self.settings = {};
-        // Set default values
-        for (var attr in defaultSettings) {
-            self.settings[attr] = defaultSettings[attr];
-        }
-        // Set new values
-        for (var attr2 in settings) {
-            self.settings[attr2] = settings[attr2];
-        }
-    };
+
     self.setElements = function() {
         var settings = self.settings,
             slider = settings.slider,
-            wrapper;
+            tmpEl = false;
 
         // Wrap slider
-        if (!settings.wrapper) {
+        if (!self.wrapper) {
             self.wrapper = wrapElement(slider);
-            self.wrapper.addClass('dk-jsu-slider-wrapper');
+            self.wrapper
+                .addClass('dk-jsu-slider-wrapper')
+                .addClass('vanilla-slider');
         }
 
         // Get slides
@@ -84,28 +83,71 @@ var vanillaSlider = function(settings) {
 
         // Add class to slider
         slider.addClass('dk-jsu-slider');
-        slider.setStyles({
-            'position': 'relative',
-            'z-index': 0
-        });
 
+        // Set slides
         for (var i = 0; i < settings.nbSlides; i++) {
             settings.slides[i].addClass('dk-jsu-slide');
-            settings.slides[i].setStyles({
-                'position': 'absolute',
-                'z-index': 0,
-                'left': 0,
-                'top': 0
-            });
         }
+
+        // Set Navigation
+        if (settings.displayNavigation && settings.createNavigation) {
+            self.navigationEl = document.createElement('div').addClass('navigation');
+            // Prev
+            tmpEl = document.createElement('div');
+            tmpEl.setAttribute('data-rel', 'prev');
+            tmpEl.addClass('prev').innerHTML = 'prev';
+            self.navigationEl.appendChild(tmpEl);
+            // Next
+            tmpEl = document.createElement('div');
+            tmpEl.setAttribute('data-rel', 'next');
+            tmpEl.addClass('next').innerHTML = 'next';
+            self.navigationEl.appendChild(tmpEl);
+            // Set navigation element
+            self.wrapper.appendChild(self.navigationEl);
+        }
+
+        // Set Pagination
+        if (settings.displayPagination && settings.createPagination) {
+            self.paginationEl = document.createElement('div').addClass('pagination');
+            // Create bullets
+            for (var j = 0; j < settings.nbSlides; j++) {
+                tmpEl = document.createElement('span');
+                tmpEl.setAttribute('data-i', j);
+                tmpEl.innerHTML = '&bull;';
+                self.paginationEl.appendChild(tmpEl);
+            }
+            // Set pagination element
+            self.wrapper.appendChild(self.paginationEl);
+            self.pagers = self.paginationEl.children;
+        }
+
     };
     self.setEvents = function() {
         var settings = self.settings,
-            slider = settings.slider;
-        slider.addEvent('click', function(e) {
-            window.eventPreventDefault(e);
-            self.goToSlide('next');
-        });
+            slider = settings.slider,
+            tmpEls, tmpEl;
+
+        // Navigation
+        if (settings.displayNavigation && self.navigationEl) {
+            self.navigationEl.children.eachElement(function(el) {
+                el.addEvent('click', function(e) {
+                    window.eventPreventDefault(e);
+                    self.goToSlide(this.getAttribute('data-rel'));
+                });
+            });
+        }
+
+        // Pagination
+        if (settings.displayPagination && self.paginationEl) {
+            self.paginationEl.children.eachElement(function(el) {
+                el.addEvent('click', function(e) {
+                    window.eventPreventDefault(e);
+                    var i = parseInt(this.getAttribute('data-i'), 10);
+                    self.goToSlide(i);
+                });
+            });
+        }
+
         // Auto slide
         self.setAutoSlide();
         slider.addEvent('mouseover', self.clearAutoSlide);
@@ -127,7 +169,7 @@ var vanillaSlider = function(settings) {
     };
     self.goToSlide = function(nb) {
         var settings = self.settings,
-            oldNb = settings.currentSlide,
+            oldNb = self.currentSlide,
             newNb = nb,
             slides = settings.slides;
 
@@ -136,11 +178,11 @@ var vanillaSlider = function(settings) {
         }
 
         if (nb === 'prev') {
-            nb = settings.currentSlide - 1;
+            nb = self.currentSlide - 1;
         }
 
         if (nb === 'next') {
-            nb = settings.currentSlide + 1;
+            nb = self.currentSlide + 1;
         }
 
         if (nb < 0) {
@@ -173,10 +215,29 @@ var vanillaSlider = function(settings) {
             });
             self.canSlide = 1;
         }
+        if (settings.displayPagination && self.pagers[nb]) {
+            self.pagers.eachElement(function(el) {
+                el.removeClass('current');
+            });
+            self.pagers[nb].addClass('current');
+        }
 
         // Current slide = asked slide
-        settings.currentSlide = nb;
+        self.currentSlide = nb;
     };
     self.init(settings);
     return self;
+};
+
+/* Get Settings */
+vanillaSlider.prototype.getSettings = function(settings) {
+    this.settings = {};
+    // Set default values
+    for (var attr in this.defaultSettings) {
+        this.settings[attr] = this.defaultSettings[attr];
+    }
+    // Set new values
+    for (var attr2 in settings) {
+        this.settings[attr2] = settings[attr2];
+    }
 };
