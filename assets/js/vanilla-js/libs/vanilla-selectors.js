a4a694d89b881a0a43d013084d988f93d75dc23d
Darklg <kev.rocher@gmail.com>
2014-04-01 10:50:17 +0200

    Vanilla - Selectors v 1.1 : extend for function find('selector') && parent selector for $$_ & $_

--- assets/js/vanilla-js/libs/vanilla-selectors.js
+++ assets/js/vanilla-js/libs/vanilla-selectors.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Selectors
- * Version: 1.0.1
+ * Version: 1.1
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -9,38 +9,46 @@
   $$_ : Get multiple elements
 ---------------------------------------------------------- */
 
-var $$_ = function(selector) {
+var $$_ = function(selector, parent) {
     var idMatch = /^\#([a-z0-9_-]+)$/,
         classMatch = /^\.([a-z0-9_-]+)$/,
         tagMatch = /^([a-z]+)$/;
 
+    parent = parent || document;
+
     // If selector looks like an ID, uses $_ for performance
     if (selector.match(idMatch)) {
-        return [document.getElementById(selector)];
+        return [parent.getElementById(selector)];
     }
 
-    // If selector looks like a CSS Class, uses $_ for performance
-    if (selector.match(classMatch)) {
-        return document.getElementsByClassName(selector);
+    // If selector looks like a CSS Class, uses getElementsByClassName for performance if available
+    if (selector.match(classMatch) && "getElementsByClassName" in document) {
+        return parent.getElementsByClassName(selector);
     }
 
     // If selector matches a tag elements, uses getElementsByTagName for performance
     if (selector.match(tagMatch)) {
-        return document.getElementsByTagName(selector);
+        return parent.getElementsByTagName(selector);
     }
 
     // If Query Selector exists, use it
     if ("querySelectorAll" in document) {
-        return document.querySelectorAll(selector);
+        return parent.querySelectorAll(selector);
     }
 
     return [];
 };
 
+if (!Element.prototype.find) {
+    Element.prototype.find = function(selector) {
+        return $$_(selector, this);
+    };
+}
 /* ----------------------------------------------------------
   $_ : Get Element
 ---------------------------------------------------------- */
 
-var $_ = function(id) {
-    return document.getElementById(id);
+var $_ = function(id, parent) {
+    parent = parent || document;
+    return parent.getElementById(id);
 };
