a02945b18fada23bda6d043b5c29bb3a11c29f3a
Darklg <kev.rocher@gmail.com>
2014-04-01 10:38:31 +0200

    Vanilla - IE v 0.2 : Small HTML5 Shim

--- assets/js/vanilla-js/libs/vanilla-ie.js
+++ assets/js/vanilla-js/libs/vanilla-ie.js
@@ -1,9 +1,22 @@
 /*
  * Plugin Name: Vanilla-JS IE
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
+
+/* ----------------------------------------------------------
+  HTML5 shim
+ ---------------------------------------------------------- */
+
+(function() {
+    var tagz = ["article", "aside", "details", "figcaption", "figure", "footer", "header", "hgroup", "nav", "section", "video", "audio", "mark"],
+        tagzLen = tagz.length,
+        i;
+    for (i = 0; i < tagzLen; i++) {
+        document.createElement(tagz[i]);
+    }
+}());
 
 /* ----------------------------------------------------------
   String
