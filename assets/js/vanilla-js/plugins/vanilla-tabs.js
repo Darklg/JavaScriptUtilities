a26ab1f307f9d1326852536e4787b1859cc07105
Darklg <kev.rocher@gmail.com>
2014-04-06 21:37:34 +0200

    Vanilla - Tabs v 0.2 : Checks and comments

--- assets/js/vanilla-js/plugins/vanilla-tabs.js
+++ assets/js/vanilla-js/plugins/vanilla-tabs.js
@@ -1,9 +1,9 @@
 /*
  * Plugin Name: Vanilla Tabs
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Tabs may be freely distributed under the MIT license.
- * Required: Vanilla Events, Vanilla Elements
+ * Required: Vanilla Events, Vanilla Elements, Vanilla Classes
  */
 
 var vanillaTabs = function(settings) {
@@ -16,13 +16,21 @@ var vanillaTabs = function(settings) {
     };
     self.init = function(settings) {
         self.getSettings(settings);
+        // Check if triggers and targets exists
         if (!self.settings.triggers || !self.settings.targets) {
             return false;
         }
         triggers = self.settings.triggers;
         targets = self.settings.targets;
+        // Prevent double launch
+        if (!triggers[0] || triggers[0].hasClass('has-vanilla-tabs')) {
+            return;
+        }
+        triggers[0].addClass('has-vanilla-tabs');
+        // Set Elements & events
         self.setElements();
         self.setEvents();
+        // Go to tab #1
         self.setTab(0);
     };
     self.setElements = function() {
@@ -35,24 +43,33 @@ var vanillaTabs = function(settings) {
         triggers.eachElement(function(el) {
             el.addEvent('click', function(e) {
                 window.eventPreventDefault(e);
-                self.setTab(parseInt(el.getAttribute('data-i'), 10));
+                // Get trigger number
+                var i = parseInt(el.getAttribute('data-i'), 10);
+                // Set called tab
+                self.setTab(i);
             });
         });
     };
     self.setTab = function(i) {
-        var settings = self.settings;
+        var classCurrent = self.settings.classCurrent,
+            callBackTab = self.settings.callBackTab;
         if (!triggers[i] || !targets[i]) {
             return;
         }
+        // Remove class on all triggers and targets
         triggers.eachElement(function(el) {
-            el.removeClass(settings.classCurrent);
+            el.removeClass(classCurrent);
         });
-        triggers[i].addClass(settings.classCurrent);
         targets.eachElement(function(el) {
-            el.removeClass(settings.classCurrent);
+            el.removeClass(classCurrent);
         });
-        targets[i].addClass(settings.classCurrent);
-        settings.callBackTab(i);
+        // Add class on current trigger & target
+        triggers[i].addClass(classCurrent);
+        targets[i].addClass(classCurrent);
+        // Launch callback
+        if (typeof callBackTab == 'function') {
+            callBackTab(i);
+        }
     };
     self.init(settings);
     return self;
