6c6613e0366f1989934bf1ac5a2eccb1f38e0753
Darklg <kev.rocher@gmail.com>
2014-04-07 23:19:51 +0200

    Vanilla - Lightbox v 0.4 : Open Image

--- assets/js/vanilla-js/plugins/vanilla-lightbox.js
+++ assets/js/vanilla-js/plugins/vanilla-lightbox.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Lightbox
- * Version: 0.3
+ * Version: 0.4
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Lightbox may be freely distributed under the MIT license.
  * Required: Vanilla Events, Vanilla Selectors, Vanilla Classes
@@ -38,11 +38,10 @@ var vanillaLightbox = function(settings) {
         document.body.appendChild(els.box);
     };
     self.setEvents = function() {
-        var keyPressed;
         self.els.filter.addEvent('click', self.close);
         window.addEvent(window, 'keydown', function(e) {
             // Close on echap
-            keyPressed = e.keyCode ? e.keyCode : e.charCode;
+            var keyPressed = e.keyCode ? e.keyCode : e.charCode;
             if (keyPressed === 27) {
                 self.close();
             }
@@ -63,11 +62,48 @@ var vanillaLightbox = function(settings) {
     self.open = function() {
         self.els.box.removeClass('lb-is-hidden');
     };
+    self.openImage = function(url) {
+        var img = new Image();
+        img.onload = function() {
+            var cStyle = self.els.content.style,
+                imgWidth = this.width,
+                imgHeight = this.height,
+                ratioImage = imgWidth / imgHeight,
+                winWidth = getWindowInnerWidth() * 0.9,
+                winHeight = getWindowInnerHeight() * 0.9,
+                ratioWindow = winHeight / winWidth;
+
+            if (ratioImage > ratioWindow) {
+                // Window is larger
+                imgHeight = Math.min(winHeight, imgHeight);
+                imgWidth = imgHeight * ratioImage;
+            }
+            else {
+                // Image is larger
+                imgWidth = Math.min(winWidth, imgWidth);
+                imgHeight = imgWidth / ratioImage;
+            }
+
+            // Set Lightbox style
+            cStyle.height = imgHeight + 'px';
+            cStyle.width = imgWidth + 'px';
+            cStyle.overflow = 'hidden';
+
+            // Load image into Lightbox
+            self.setContent('<img src="' + url + '" alt="" />');
+
+            // Open
+            self.open();
+        };
+        img.src = url;
+    };
     self.close = function(e) {
         if (e) {
             window.eventPreventDefault(e);
         }
         self.els.box.addClass('lb-is-hidden');
+        // Reset style
+        self.els.content.setAttribute('style', '');
     };
     self.init(settings);
     return self;
