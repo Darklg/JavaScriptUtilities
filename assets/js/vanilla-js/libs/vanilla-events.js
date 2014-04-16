7947d9533bcd448b7a9b3f267e62f753d700b5cf
Darklg <kev.rocher@gmail.com>
2014-04-16 21:12:18 +0200

    Vanilla - Events v 1.2 : Trigger Event

--- assets/js/vanilla-js/libs/vanilla-events.js
+++ assets/js/vanilla-js/libs/vanilla-events.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Events
- * Version: 1.1.1
+ * Version: 1.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -42,6 +42,34 @@ if (!Element.prototype.addEvent) {
         return window.addEvent(this, eventName, callback);
     };
 }
+
+/* ----------------------------------------------------------
+  Trigger event
+---------------------------------------------------------- */
+
+window.triggerEvent = function(el, eventName) {
+    var e = false;
+    if (document.createEventObject) {
+        e = document.createEventObject();
+        e.button = 1;
+        return el.fireEvent("on" + eventName, e);
+    }
+    else {
+        e = document.createEvent("HTMLEvents");
+        e.initEvent(eventName, true, false);
+        return el.dispatchEvent(e);
+    }
+};
+
+if (!Element.prototype.triggerEvent) {
+    Element.prototype.triggerEvent = function(eventName) {
+        return window.triggerEvent(this, eventName);
+    };
+}
+
+/* ----------------------------------------------------------
+  Event prevent default
+---------------------------------------------------------- */
 
 window.eventPreventDefault = function(event) {
     return (event.preventDefault) ? event.preventDefault() : event.returnValue = false;
