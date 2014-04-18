4fcfb78f0a0fddb02c9fa7cf2b68cb2f91266e61
Darklg <kev.rocher@gmail.com>
2014-04-19 00:58:42 +0200

    Vanilla - Template v 0.2 : Try to load settings from element

--- assets/js/vanilla-js/template.js
+++ assets/js/vanilla-js/template.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Template
- * Version: 0.1.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -25,9 +25,19 @@ var replaceThisStringWithAnID = function(settings) {
 
 /* Get Settings */
 replaceThisStringWithAnID.prototype.getSettings = function(settings) {
+    var nSettings;
     if (typeof settings != 'object') {
         settings = {};
     }
+    (function() {
+        if ('loadElement' in settings && settings.loadElement.getAttribute('data-settings')) {
+            nSettings = JSON.parse(settings.loadElement.getAttribute('data-settings'));
+            if (typeof nSettings == 'object') {
+                nSettings.slider = settings.loadElement;
+                settings = nSettings;
+            }
+        }
+    }());
     this.settings = {};
     // Set default values
     for (var attr in this.defaultSettings) {
