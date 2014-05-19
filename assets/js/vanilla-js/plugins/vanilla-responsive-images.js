52493bf1fafdc875f45c17fcdb18834fe7db057a
Darklg <kev.rocher@gmail.com>
2014-05-19 23:13:08 +0200

    Vanilla - Responsive images v 0.2
    * Cleaner code. Yep.
    * Set image only after success of loading, to avoid white flashes.
    * Set best image available on resize.

--- assets/js/vanilla-js/plugins/vanilla-responsive-images.js
+++ assets/js/vanilla-js/plugins/vanilla-responsive-images.js
@@ -1,17 +1,130 @@
 /*
  * Plugin Name: Responsive Images
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Responsive Images may be freely distributed under the MIT license.
  * Usage status: Work in progress
  */
 
-
 var vanillaResponsiveImages = function(el, property) {
-    var self = this;
-    if (!property) {
-        property = 'background';
-    }
+    var self = this,
+        values = false,
+        srcset = false,
+        timer = false;
+
+    self.init = function() {
+        if (!property) {
+            property = 'background';
+        }
+        // Get srcset
+        srcset = el.getAttribute('data-srcset');
+        if (!srcset) {
+            return false;
+        }
+        // Get image values
+        values = self.extractValues(srcset);
+        if (!values) {
+            return false;
+        }
+
+        // Launch responsive image
+        self.setResponsiveImage();
+
+        // Launch responsive image on resize end
+        if ('addEventListener' in window) {
+            window.addEventListener('resize', self.timerResponsiveImage);
+            window.addEventListener('orientationchange', self.timerResponsiveImage);
+        }
+    };
+
+    self.setResponsiveImage = function() {
+        // Extract from srcset values
+        var imgValue = self.getFinalImageFromValues(values);
+
+        // load latest ok image
+        if (imgValue !== false) {
+            self.setUrl(imgValue);
+        }
+    };
+
+    self.timerResponsiveImage = function() {
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+            self.setResponsiveImage();
+        }, 1000);
+    };
+
+    self.extractValues = function(srcset) {
+        var values = [],
+            match = false,
+            regexp = /([\S]+)\s([0-9]{1,4})([w,h])/,
+            tmpSrcset = srcset.split(',');
+        for (var i in tmpSrcset) {
+            if (tmpSrcset[i] === '' || !tmpSrcset.hasOwnProperty(i)) {
+                continue;
+            }
+            match = tmpSrcset[i].match(regexp);
+            if (match) {
+                values.push({
+                    url: match[1],
+                    dim: parseInt(match[2], 10),
+                    dir: match[3],
+                });
+            }
+        }
+        return values;
+    };
+
+    self.getFinalImageFromValues = function(values) {
+        var checkDim = false,
+            imgValue = false,
+            winWidth = getWindowInnerWidth(),
+            winHeight = getWindowInnerHeight(),
+            i = false;
+        // Parse values
+        for (i in values) {
+            // Test window width
+            if (values[i].dir == 'w' && values[i].dim < winWidth) {
+                checkDim = true;
+            }
+
+            // Test window height
+            if (values[i].dir == 'h' && values[i].dim < winHeight) {
+                checkDim = true;
+            }
+
+            // If test ok, we can load this image
+            if (checkDim) {
+                imgValue = values[i].url;
+            }
+            checkDim = false;
+
+        }
+        return imgValue;
+    };
+
+    self.setUrl = function(imgValue) {
+        var img = document.createElement('img'),
+            origImgValue = imgValue;
+
+        img.onload = function() {
+            // Set src
+            if (property == 'src') {
+                el.src = imgValue;
+                return true;
+            }
+            // Set CSS property
+            if (property == 'background' || property == 'backgroundImage') {
+                imgValue = 'url(' + imgValue + ')';
+            }
+            el.style[property] = imgValue;
+        };
+        img.src = origImgValue;
+
+        // Kill function : image is loaded
+        return true;
+    };
+
     // Utilities
     var getWindowInnerHeight = function() {
         return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
@@ -20,80 +133,7 @@ var vanillaResponsiveImages = function(el, property) {
         return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 0;
     };
 
-    var setUrl = function(imgValue) {
+    self.init();
 
-        // Set src
-        if (property == 'src') {
-            el.src = imgValue;
-            return true;
-        }
-
-        // Set CSS property
-        if (property == 'background' || property == 'backgroundImage') {
-            imgValue = 'url(' + imgValue + ')';
-        }
-        el.style[property] = imgValue;
-
-        // Kill function : image is loaded
-        return true;
-    };
-
-    // Get window width and height
-    var winWidth = getWindowInnerWidth(),
-        winHeight = getWindowInnerHeight(),
-        srcset = el.getAttribute('data-srcset'),
-        values = [],
-        tmpSrcset = [],
-        i, match, checkDim, val, imgValue = false;
-
-    if (!srcset) {
-        return false;
-    }
-
-    // Extract srcset values
-    tmpSrcset = srcset.split(',');
-    for (i in tmpSrcset) {
-        if (tmpSrcset[i] === '' || !tmpSrcset.hasOwnProperty(i)) {
-            continue;
-        }
-        match = tmpSrcset[i].match(/([\S]+)\s([0-9]{1,4})([w,h])/);
-        if (match) {
-            values.push({
-                url: match[1],
-                dim: parseInt(match[2], 10),
-                dir: match[3],
-            });
-        }
-    }
-
-    if (!values) {
-        return false;
-    }
-
-    // Parse values
-    for (i in values) {
-        val = values[i];
-        checkDim = false;
-        // Test window width
-        if (val.dir == 'w' && val.dim < winWidth) {
-            checkDim = true;
-        }
-
-        // Test window height
-        if (val.dir == 'h' && val.dim < winHeight) {
-            checkDim = true;
-        }
-
-        // If test ok, we can load this image
-        if (checkDim) {
-            imgValue = val.url;
-        }
-    }
-
-    // load latest ok image
-    if (imgValue !== false) {
-        setUrl(imgValue);
-    }
-
-    return this;
+    return self;
 };
