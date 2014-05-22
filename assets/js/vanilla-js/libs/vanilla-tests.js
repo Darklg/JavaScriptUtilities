f5f00f68dce7273bf511c7f7a6d4d2175cc686ed
Darklg <kev.rocher@gmail.com>
2014-05-22 18:48:31 +0200

    Vanilla - tests v 1.1 : Get navigator basic & toucheend event

--- assets/js/vanilla-js/libs/vanilla-tests.js
+++ assets/js/vanilla-js/libs/vanilla-tests.js
@@ -1,11 +1,25 @@
 /*
  * Plugin Name: Vanilla-JS Tests
- * Version: 1.0
+ * Version: 1.1
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
 
 var dkJSUTests = {
+    // Navigator
+    getNavigator: function() {
+        var nua = navigator.userAgent,
+            navInfos = {
+                os: false,
+                name: false,
+                version: false
+            };
+        if (nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1 && nua.indexOf('Chrome') === -1) {
+            navInfos.os = 'android';
+            navInfos.name = 'native-android';
+        }
+    },
+    // Capacities
     canvas: function() {
         var elem = document.createElement('canvas');
         return !!(elem.getContext && elem.getContext('2d'));
@@ -24,4 +38,7 @@ var dkJSUTests = {
     touch: function() {
         return 'ontouchstart' in window || 'onmsgesturechange' in window;
     },
+    touchendEvent: function() {
+        return window.navigator.msPointerEnabled ? "MSPointerUp" : "touchend";
+    }
 };
