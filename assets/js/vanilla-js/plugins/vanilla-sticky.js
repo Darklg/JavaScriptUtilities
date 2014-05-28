16d2767fcacee18fa722ce1ff463af52fcbbfe72
Darklg <kev.rocher@gmail.com>
2014-05-28 21:06:38 +0200

    Vanilla - Sticky v 0.4 : Better calc, resize calc, margin:0 on element by default, demo

--- assets/js/vanilla-js/plugins/vanilla-sticky.js
+++ assets/js/vanilla-js/plugins/vanilla-sticky.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla Sticky
- * Version: 0.3
+ * Version: 0.4
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Sticky may be freely distributed under the MIT license.
  * Usage status: Work in progress
@@ -8,7 +8,7 @@
 
 var vanillaSticky = function(el, options) {
 
-    var origElTop = el.offsetTop,
+    var origElTop = false,
         maxScroll = false,
         maxTop = false,
         wrapper = false;
@@ -18,32 +18,58 @@ var vanillaSticky = function(el, options) {
     }
 
     function init() {
+
         // Set initial style
         el.style.position = 'relative';
+        el.style.margin = '0';
 
         if (options.stickyParent) {
             options.stickyParent.style.position = 'relative';
-            maxTop = options.stickyParent.clientHeight - el.clientHeight;
-            maxScroll = options.stickyParent.offsetTop + maxTop;
         }
 
         // Create parent node to avoid layout change
         wrapper = document.createElement('div');
         el.parentNode.insertBefore(wrapper, el);
         wrapper.appendChild(el);
-        wrapper.style.minHeight = wrapper.clientHeight + 'px';
 
         // Launch events
         launchEvents();
     }
 
+    function calcScroll() {
+
+        // Get element offset
+        var refnode = el;
+        origElTop = refnode.offsetTop;
+        while (origElTop === 0) {
+            refnode = refnode.parentNode;
+            if (!refnode) {
+                break;
+            }
+            origElTop = refnode.parentNode.offsetTop;
+        }
+        // Get parent scroll dimensions
+        if (options.stickyParent) {
+            maxTop = options.stickyParent.clientHeight - el.clientHeight;
+            maxScroll = options.stickyParent.offsetTop + maxTop;
+        }
+        wrapper.style.minHeight = wrapper.clientHeight + 'px';
+    }
+
     function launchEvents() {
+
+        // Resize event
+        calcScroll();
         // Scroll event
+        listenScroll();
+
         if (window.addEventListener) {
             window.addEventListener('scroll', listenScroll);
+            window.addEventListener('resize', calcScroll);
         }
         else if (window.attachEvent) {
             window.attachEvent("onscroll", listenScroll);
+            window.attachEvent("onresize", calcScroll);
         }
     }
 
