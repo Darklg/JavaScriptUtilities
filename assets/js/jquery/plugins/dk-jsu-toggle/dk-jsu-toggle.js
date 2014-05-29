3333dc10a7482f80719d1109666e558e2e2b80e4
Darklg <kev.rocher@gmail.com>
2014-05-29 20:11:42 +0200

    jQuery - Toggle v 0.2 : Close on outside click

--- assets/js/jquery/plugins/dk-jsu-toggle/dk-jsu-toggle.js
+++ assets/js/jquery/plugins/dk-jsu-toggle/dk-jsu-toggle.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Toggle
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Toggle may be freely distributed under the MIT license.
  */
@@ -18,23 +18,34 @@ if (!jQuery.fn.dkJSUToggle) {
         // Main Class
         var dkJSUToggle = {
             init: function(el) {
+                this.$body = $('body');
+                this.isOpen = false;
                 this.el = el;
                 this.setToggler();
                 this.setEvents();
             },
             setToggler: function() {
                 this.elTarget = this.el.parent();
-                if (this.el.data('dkjsutoggle-closest')) {
-                    this.elTarget = this.el.closest(this.el.data('dkjsutoggle-closest'));
+                if (this.el.data('jqtoggle-closest')) {
+                    this.elTarget = this.el.closest(this.el.data('jqtoggle-closest'));
                 }
                 this.toggleClass = 'is-active';
             },
             // Setting events
             setEvents: function() {
                 var self = this;
+                // Toggle on click
                 self.el.on('click', function(e) {
                     e.preventDefault();
                     self.elTarget.toggleClass(self.toggleClass);
+                    self.isOpen = self.elTarget.hasClass(self.toggleClass);
+                });
+                // Close on outside click
+                self.$body.on('click', function(e) {
+                    if (self.isOpen && !self.el.is(e.target) && self.el.has(e.target).length === 0) {
+                        self.elTarget.removeClass(self.toggleClass);
+                        self.isOpen = false;
+                    }
                 });
             }
         };
