6343a5fa2f4e9a98ff1f7fd201ccc3af97678252
Darklg <kev.rocher@gmail.com>
2014-09-13 21:43:25 +0200

    Vanilla JS - GetUserMedia v 0.2 : Allow custom callbacks

--- assets/js/vanilla-js/libs/vanilla-getusermedia.js
+++ assets/js/vanilla-js/libs/vanilla-getusermedia.js
@@ -1,22 +1,31 @@
 /*
  * Plugin Name: Vanilla-JS getUserMedia
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
-
 var dkJSUGetUserMedia = function(target, params) {
-
-    var userMediaParams = {};
+    var userMediaParams = {},
+        successCallback = function(target, localMediaStream) {
+            target.src = window.URL.createObjectURL(localMediaStream);
+            target.play();
+        },
+        failureCallback = function(target) {
+            target.src = '';
+        },
+        completeCallback = function(localMediaStream) {
+            successCallback(target, localMediaStream);
+        },
+        errorCallback = function(err) {
+            if (err) {
+                console.log("Error : " + err);
+            }
+            failureCallback(target);
+        };
 
     /* Try to get getUserMedia */
     if (!navigator.hasOwnProperty('getUserMedia')) {
         navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-    }
-
-    /* If we can't reach it, stop the function */
-    if (!navigator.getUserMedia) {
-        return false;
     }
 
     /* Try to get params */
@@ -28,24 +37,27 @@ var dkJSUGetUserMedia = function(target, params) {
     }
 
     /* Set stream params */
-
+    /* - Media type */
     for (var id in params.type) {
         userMediaParams[params.type[id]] = true;
     }
+    /* - Callbacks */
+    if (params.hasOwnProperty('successCallback') && typeof params.successCallback == 'function') {
+        successCallback = params.successCallback;
+    }
+    if (params.hasOwnProperty('errorCallback') && typeof params.errorCallback == 'function') {
+        errorCallback = params.errorCallback;
+    }
 
-    /* Define callbacks */
-
-    var successCallback = function(localMediaStream) {
-        target.src = window.URL.createObjectURL(localMediaStream);
-        target.play();
-    };
-    var errorCallback = function(err) {
-        console.log("The following error occured: " + err);
-    };
+    /* If we can't reach it, stop the function */
+    if (!navigator.getUserMedia) {
+        failureCallback(target);
+        return false;
+    }
 
     /* Launch call */
 
-    navigator.getUserMedia(userMediaParams, successCallback, errorCallback);
+    navigator.getUserMedia(userMediaParams, completeCallback, errorCallback);
 
     return true;
 };
