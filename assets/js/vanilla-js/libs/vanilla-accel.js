0e2fe89204e803e1d7609868b841fc443bba1fba
Darklg <kev.rocher@gmail.com>
2014-12-11 20:34:07 +0100

    Vanilla - Accelerometer v 0.2 : Load initial values to use as an offset

--- assets/js/vanilla-js/libs/vanilla-accel.js
+++ assets/js/vanilla-js/libs/vanilla-accel.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Accelerometer
- * Version: 0.1
+ * Version: 0.2
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -8,11 +8,14 @@
 var dkJSUAccelerometer = function(item, settings) {
     var self = this;
     self.defaultSettings = {
-        property: 'transform3d'
+        property: 'transform3d',
+        startAtInitialPosition: true
     };
     self.properties = {
-        x: 0,
-        y: 0
+        offsetX: false,
+        offsetY: false,
+        x: false,
+        y: false
     };
     self.init = function(item, settings) {
         if (!item) {
@@ -23,28 +26,39 @@ var dkJSUAccelerometer = function(item, settings) {
         self.setEvents();
     };
     self.setEvents = function() {
-        // Cancel if accelerometer is not supporterd
+        // Cancel if accelerometer is not supported
         if (!window.DeviceOrientationEvent) {
             return;
         }
         window.addEventListener('deviceorientation', function(e) {
+            // Load initial values to use as an offset
+            if (self.properties.offsetX === false) {
+                self.properties.offsetX = e.gamma;
+                self.properties.offsetY = e.beta;
+            }
             self.properties.x = e.gamma;
             self.properties.y = e.beta;
             self.triggerPropertyChange();
         }, false);
     };
     self.triggerPropertyChange = function() {
-        var x = self.properties.x,
-            y = self.properties.y;
+        var x = 0 - self.properties.x,
+            y = 0 - self.properties.y;
+
+        if (self.settings.startAtInitialPosition) {
+            x += self.properties.offsetX;
+            y += self.properties.offsetY;
+        }
+
         switch (self.settings.property) {
             case 'transform3d':
-                var t = 'translate(' + (0 - x) + 'px, ' + (0 - y) + 'px)';
+                var t = 'translate(' + x + 'px, ' + y + 'px)';
                 self.item.style.WebkitTransform = t;
                 self.item.style.MozTransform = t;
                 self.item.style.transform = t;
                 break;
             case 'background-position':
-                self.item.style.backgroundPosition = (0 - x) + 'px ' + (0 - y) + 'px';
+                self.item.style.backgroundPosition = x + 'px ' + y + 'px';
                 break;
             default:
         }
