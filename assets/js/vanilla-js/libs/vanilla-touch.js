43590644d375b42bf074cd4e7f736e3f1c6e2e18
Darklg <kev.rocher@gmail.com>
2016-08-28 23:31:49 +0200

    Vanilla - Touch v 0.6.1 : Remove Hybrid Screen plugin

--- assets/js/vanilla-js/libs/vanilla-touch.js
+++ assets/js/vanilla-js/libs/vanilla-touch.js
@@ -1,6 +1,6 @@
 /*
  * Plugin Name: Vanilla-JS Touch
- * Version: 0.6
+ * Version: 0.6.1
  * Plugin URL: https://github.com/Darklg/JavaScriptUtilities
  * JavaScriptUtilities Vanilla-JS may be freely distributed under the MIT license.
  */
@@ -223,30 +223,5 @@ var setSwipeEvent = function(el, options) {
         if ("jQuery" in window) {
             jQuery(el).trigger(eventTriggered);
         }
-    }
-};
-
-/* ----------------------------------------------------------
-  Detect touch / mouse hybrid screens
----------------------------------------------------------- */
-
-var vanillaDetectHybridScreens = function() {
-    var currentScreenMode = '';
-    window.addEventListener('mousemove', function(e) {
-        if (currentScreenMode == 'mouse') {
-            return;
-        }
-        setCurrentScreenMode('mouse');
-    }, false);
-    window.addEventListener('touchstart', function(e) {
-        if (currentScreenMode == 'touch') {
-            return;
-        }
-        setCurrentScreenMode('touch');
-    }, false);
-
-    function setCurrentScreenMode(mode) {
-        currentScreenMode = mode;
-        document.body.setAttribute('data-inputmode', mode);
     }
 };
